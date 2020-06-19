a2171920fbc75c23ef9d4cb5382d1009f6129f28
MIDET-tech <63294970+MIDET-tech@users.noreply.github.com>
2020-06-19 22:33:15 +0900

    Initial commit

---
+++ module/module.py
@@ -0,0 +1,15 @@
+# Summary of modules
+
+import time
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class modules:
+  def __init__(self):
+    self.driver = webdriver.Chrome()
+
+  def 
